d0f457ead02669bf4ea141002dbd10d305507b12
Yicheng Luo <ethanluoyc@gmail.com>
2022-06-04 22:48:24 +0100

    Update vae.py

--- vae.py
+++ vae.py
@@ -7,31 +7,23 @@ from torchvision import transforms
 import torch.optim as optim
 from torch import nn
 import matplotlib.pyplot as plt
-
-
-class Normal(object):
-    def __init__(self, mu, sigma, log_sigma, v=None, r=None):
-        self.mu = mu
-        self.sigma = sigma  # either stdev diagonal itself, or stdev diagonal from decomposition
-        self.logsigma = log_sigma
-        dim = mu.get_shape()
-        if v is None:
-            v = torch.FloatTensor(*dim)
-        if r is None:
-            r = torch.FloatTensor(*dim)
-        self.v = v
-        self.r = r
-
+from torch import distributions
 
 class Encoder(torch.nn.Module):
-    def __init__(self, D_in, H, D_out):
+    def __init__(self, D_in, H, latent_size):
         super(Encoder, self).__init__()
         self.linear1 = torch.nn.Linear(D_in, H)
-        self.linear2 = torch.nn.Linear(H, D_out)
+        self.linear2 = torch.nn.Linear(H, H)
+        self.enc_mu = torch.nn.Linear(H, latent_size)
+        self.enc_log_sigma = torch.nn.Linear(H, latent_size)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
-        return F.relu(self.linear2(x))
+        x = F.relu(self.linear2(x))
+        mu = self.enc_mu(x)
+        log_sigma = self.enc_log_sigma(x)
+        sigma = torch.exp(log_sigma)
+        return torch.distributions.Normal(loc=mu, scale=sigma)
 
 
 class Decoder(torch.nn.Module):
@@ -39,82 +31,70 @@ class Decoder(torch.nn.Module):
         super(Decoder, self).__init__()
         self.linear1 = torch.nn.Linear(D_in, H)
         self.linear2 = torch.nn.Linear(H, D_out)
+        
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
-        return F.relu(self.linear2(x))
-
+        mu = torch.tanh(self.linear2(x))
+        return torch.distributions.Normal(mu, torch.ones_like(mu))
 
 class VAE(torch.nn.Module):
-    latent_dim = 8
-
     def __init__(self, encoder, decoder):
         super(VAE, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
-        self._enc_mu = torch.nn.Linear(100, 8)
-        self._enc_log_sigma = torch.nn.Linear(100, 8)
-
-    def _sample_latent(self, h_enc):
-        """
-        Return the latent normal sample z ~ N(mu, sigma^2)
-        """
-        mu = self._enc_mu(h_enc)
-        log_sigma = self._enc_log_sigma(h_enc)
-        sigma = torch.exp(log_sigma)
-        std_z = torch.from_numpy(np.random.normal(0, 1, size=sigma.size())).float()
-
-        self.z_mean = mu
-        self.z_sigma = sigma
-
-        return mu + sigma * Variable(std_z, requires_grad=False)  # Reparameterization trick
 
     def forward(self, state):
-        h_enc = self.encoder(state)
-        z = self._sample_latent(h_enc)
-        return self.decoder(z)
+        q_z = self.encoder(state)
+        z = q_z.rsample()
+        return self.decoder(z), q_z
 
 
-def latent_loss(z_mean, z_stddev):
-    mean_sq = z_mean * z_mean
-    stddev_sq = z_stddev * z_stddev
-    return 0.5 * torch.mean(mean_sq + stddev_sq - torch.log(stddev_sq) - 1)
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     # Normalize the images to be -0.5, 0.5
+     transforms.Normalize(0.5, 1)]
+    )
+mnist = torchvision.datasets.MNIST('./', download=True, transform=transform)
 
+input_dim = 28 * 28
+batch_size = 128
+num_epochs = 100
+learning_rate = 0.001
+hidden_size = 512
+latent_size = 8
 
-if __name__ == '__main__':
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
-    input_dim = 28 * 28
-    batch_size = 32
+dataloader = torch.utils.data.DataLoader(
+    mnist, batch_size=batch_size,
+    shuffle=True, 
+    pin_memory=torch.cuda.is_available())
 
-    transform = transforms.Compose(
-        [transforms.ToTensor()])
-    mnist = torchvision.datasets.MNIST('./', download=True, transform=transform)
+print('Number of samples: ', len(mnist))
 
-    dataloader = torch.utils.data.DataLoader(mnist, batch_size=batch_size,
-                                             shuffle=True, num_workers=2)
+encoder = Encoder(input_dim, hidden_size, latent_size)
+decoder = Decoder(latent_size, hidden_size, input_dim)
 
-    print('Number of samples: ', len(mnist))
+vae = VAE(encoder, decoder).to(device)
 
-    encoder = Encoder(input_dim, 100, 100)
-    decoder = Decoder(8, 100, input_dim)
-    vae = VAE(encoder, decoder)
-
-    criterion = nn.MSELoss()
-
-    optimizer = optim.Adam(vae.parameters(), lr=0.0001)
-    l = None
-    for epoch in range(100):
-        for i, data in enumerate(dataloader, 0):
-            inputs, classes = data
-            inputs, classes = Variable(inputs.resize_(batch_size, input_dim)), Variable(classes)
-            optimizer.zero_grad()
-            dec = vae(inputs)
-            ll = latent_loss(vae.z_mean, vae.z_sigma)
-            loss = criterion(dec, inputs) + ll
-            loss.backward()
-            optimizer.step()
-            l = loss.data[0]
-        print(epoch, l)
-
-    plt.imshow(vae(inputs).data[0].numpy().reshape(28, 28), cmap='gray')
-    plt.show(block=True)
+optimizer = optim.Adam(vae.parameters(), lr=learning_rate)
+for epoch in range(num_epochs):
+    for data in dataloader:
+        inputs, _ = data
+        inputs = inputs.view(-1, input_dim).to(device)
+        optimizer.zero_grad()
+        p_x, q_z = vae(inputs)
+        log_likelihood = p_x.log_prob(inputs).sum(-1).mean()
+        kl = torch.distributions.kl_divergence(
+            q_z, 
+            torch.distributions.Normal(0, 1.)
+        ).sum(-1).mean()
+        loss = -(log_likelihood - kl)
+        loss.backward()
+        optimizer.step()
+        l = loss.item()
+    print(epoch, l, log_likelihood.item(), kl.item())
